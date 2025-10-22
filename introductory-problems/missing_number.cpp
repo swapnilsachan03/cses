2984fccf991ad30fb20b45d5e8523681cfa8f0e7
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-10-22 19:55:42 +0530

    added approaches for missing number problem

--- introductory-problems/missing_number.cpp
+++ introductory-problems/missing_number.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define vi vector<int>
-#define vii vector<vector<int>>
-#define pi pair<int, int>
-#define pii vector<pair<int, int>>
+#define ll long long
 
-int main()
-{
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vii vector<pair<int, int>>
+
+void setApproach() {
   int n;
   cin >> n;
 
   unordered_set<int> st;
   int x;
 
-  for (int i = 0; i < n - 1; i++) {
+  for (int i = 1; i < n; i++) {
     cin >> x;
     st.insert(x);
   }
@@ -22,6 +23,41 @@ int main()
   for (int i = 1; i <= n; i++) {
     if (st.find(i) == st.end()) cout << i << endl;
   }
+}
 
+void xorApproach() {
+  int n;
+  cin >> n;
+
+  int x, xr = 0;
+
+  for (int i = 1; i < n; i++) {
+    cin >> x;
+    xr = xr ^ x ^ i;
+  }
+
+  xr ^= n;
+
+  cout << xr << endl;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  int x;
+  ll sum = 0, reqSum = n * (n + 1) / 2;
+
+  for (int i = 0; i < n - 1; i++) {
+    cin >> x;
+    sum += x;
+  }
+
+  cout << reqSum - sum << endl;
+}
+
+int main()
+{
+  solve();
   return 0;
 }

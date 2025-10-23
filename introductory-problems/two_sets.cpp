d575d9103aaadae0a8441410c2ec7f8c74ea2086
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-10-23 07:32:09 +0530

    wrote optimal solution for two sets problem

--- introductory-problems/two_sets.cpp
+++ introductory-problems/two_sets.cpp
@@ -1,60 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define vi vector<int>
-#define vii vector<vector<int>>
-#define pi pair<int, int>
-#define pii vector<pair<int, int>>
+#define ll long long
+#define ld long double
 
-int main()
-{
-  long long n;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vii vector<pair<int, int>>
+
+void solve() {
+  ll n;
   cin >> n;
 
-  long long sum = (n * (n +1)) / 2;
+  ll sum = (n * (n + 1)) / 2;
 
   if (sum % 2 == 1) {
     cout << "NO" << endl;
+    return;
   }
 
-  else {
-    cout << "YES" << endl;
+  sum /= 2;
 
-    vector<int> a, b;
-    vector<int> vis (n + 1, 0);
+  vector<int> a, b;
 
-    long long sum_a = 0;
-    long long max_e = n;
-
-    while (sum_a < sum / 2) {
-      long long rem = sum / 2 - sum_a;
-
-      if (max_e < rem) {
-        a.push_back(max_e);
-        sum_a += max_e;
-        vis[max_e] = 1;
-        max_e -= 1;
-      }
-
-      else {
-        a.push_back(rem);
-        vis[rem] = 1;
-        sum_a = sum / 2;
-      }
+  for (int i = n; i >= 1; i--) {
+    if (sum >= i) {
+      sum -= i;
+      a.push_back(i);
+    } else {
+      b.push_back(i);
     }
-
-    for (int i = 1; i <= n; i++) {
-      if (vis[i] == 0) b.push_back(i);
-    }
-
-    cout << a.size() << endl;
-    for (int i = 0; i < a.size(); i++) cout << a[i] << " ";
-    cout << endl;
-
-    cout << b.size() << endl;
-    for (int i = 0; i < b.size(); i++) cout << b[i] << " ";
-    cout << endl;
   }
 
+  cout << "YES" << endl;
+
+  cout << a.size() << endl;
+  for (int i = 0; i < a.size(); i++) cout << a[i] << " ";
+  cout << endl;
+
+  cout << b.size() << endl;
+  for (int i = 0; i < b.size(); i++) cout << b[i] << " ";
+  cout << endl;
+}
+
+int32_t main()
+{
+  solve();
   return 0;
 }

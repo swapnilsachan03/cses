55bb4e9f8fd4fef94b076e2a750a138dc3758111
swapnilsachan03 <swapnil.sachan2003@gmail.com>
2025-10-24 07:51:50 +0530

    added explanation for coin piles

--- introductory-problems/coin_piles.cpp
+++ introductory-problems/coin_piles.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+
+Suppose in a given query, there are A and B coins in the piles respectively. Let's say, to empty the first pile, we've to remove 2 coins X number of times and 1 coin Y number of times. Now when we do this, we'll also need to remove coins from the second pile, 1 coin X number of times and 2 coins Y number of times. This gives us:
+
+2 * X + 1 * Y = A  (equation 1)
+1 * X + 2 * Y = B  (equation 2)
+
+Simplifying the above equations, we get:
+
+X = (2 * A - B) / 3
+Y = (2 * B - A) / 3
+
+So, to check whether it is possible to empty both the piles, (2 * A - B) should be positive and divisible by 3 and (2 * B - A) should pe positive and divisible by 3.
+
+*/
+
 int main()
 {
   int t;
